cb833f2e75f40fe19c3141044a5619e5fac1add6
paranoe <is-21-4-6@permaviat.ru>
2024-12-06 10:43:17 +0500

    Написан метод ConnectServer. Дружинин

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -115,6 +116,22 @@ namespace Server
                 int Duration = (int)DateTime.Now.Subtract(client.DateConnect).TotalSeconds;
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine($"Client: {client.Token}, time connection: {client.DateConnect.ToString("HH:mm:ss dd.MM")}, duration: {Duration}");
+            }
+        }
+        static void ConnectServer()
+        {
+            IPEndPoint EndPoint = new IPEndPoint(ServerIPAddress, ServerPort);
+            Socket SocketListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            SocketListener.Bind(EndPoint);
+            SocketListener.Listen(MaxClient);
+            while (true)
+            {
+                Socket Handler = SocketListener.Accept();
+                byte[] bytes = new byte[10485760];
+                int byteRec = Handler.Receive(bytes);
+                string Message = Encoding.UTF8.GetString(bytes, 0, byteRec);
+                string Response = SetCommandClient(Message);
+                Handler.Send(Encoding.UTF8.GetBytes(Response));
             }
         }
     }
