bbfe2473ad162c96de8c1870796e84650633a26d
paranoe <is-21-4-6@permaviat.ru>
2024-12-06 10:36:25 +0500

    Написан метод SetCommandClient. Дружинин

--- Server/Program.cs
+++ Server/Program.cs
@@ -83,5 +83,28 @@ namespace Server
                 default: if (Command.Contains("/disconnect")) DisconnectServer(Command); break;
             }
         }
+        static string SetCommandClient(string Command)
+        {
+            if (Command == "/token")
+                if (AllClients.Count < MaxClient)
+                {
+                    var newClient = new Client();
+                    AllClients.Add(newClient);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"New client connection: {newClient.Token}");
+                    return newClient.Token;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("There isn't enough space on the license server");
+                    return "/limit";
+                }
+            else
+            {
+                var Client = AllClients.Find(x => x.Token == Command);
+                return Client != null ? "/connect" : "/disconnect";
+            }
+        }
     }
 }
